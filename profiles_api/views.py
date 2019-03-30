cbf90cc2ce2e186021d1e2507b929a2f9b3b7dd5
ksvkabra@gmail.com <ksvkabra@gmail.com>
2019-03-30 13:24:26 +0530

    user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from . import serializers
+from . import models
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -95,3 +96,10 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
