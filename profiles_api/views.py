ccd4fe29c920e4809b477e7e36db2461676fdc2f
ksvkabra@gmail.com <ksvkabra@gmail.com>
2019-03-30 21:57:25 +0530

    filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
+from rest_framework import authentication
 from rest_framework import viewsets
 from . import serializers
 from . import models
+from . import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -103,3 +107,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """checks email and password and return auth token"""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+
+        return ObtainAuthToken().post(request)
